3276899aa6ada206c04928b2e3b380c4f045273f
Abdallahsalhab000 <hfghgdgg018@gmail.com>
2025-10-20 22:14:04 +0300

    Create hw1.py

---
+++ abdallah/hw1.py
@@ -0,0 +1,14 @@
+import math
+
+def main():
+    x = float(input("Enter a number x: "))
+
+    y = float(input("Enter a number y: "))
+
+    print("The result of x / y is:", x / y)
+
+    print("The value of x^y is:", x ** y)
+
+    print("The value of log2(x) is:", math.log2(x))
+
+main()
